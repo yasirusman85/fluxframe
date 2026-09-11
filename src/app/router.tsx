98b42f7c6308efedcd3dff34074bbed68ff9a9f8
yasirusman85 <140712050+yasirusman85@users.noreply.github.com>
2026-09-11 18:58:10 +0500

    feat: add GitHub Pages deployment workflow with HashRouter and 404 redirect

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { Layout } from "../components/app-shell/Layout";
 import { ExplorePage } from "../features/explore/ExplorePage";
 import { ImageStudioPage } from "../features/image-studio/ImageStudioPage";
@@ -6,7 +6,7 @@ import { VideoStudioPage } from "../features/video-studio/VideoStudioPage";
 import { ProjectLibraryPage } from "../features/projects/ProjectLibraryPage";
 import { ProjectDetailPage } from "../features/projects/ProjectDetailPage";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
